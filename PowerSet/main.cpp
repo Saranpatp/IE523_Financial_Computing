305da5162c21c943f61b6881857c4a16f10f20fa
Saranpat Prasertthum <saranpatp@gmail.com>
2022-09-14 00:06:49 -0500

    fix powerset

--- PowerSet/main.cpp
+++ PowerSet/main.cpp
@@ -9,25 +9,26 @@ int main(int argc,char* argv[]) {
     int *array;
 
     sscanf(argv[1],"%d",&size_of_set);
-
     array = new int[size_of_set];
-    for (int i = 0; i<size_of_set; i++){
-        array[i]=i+1;
+    for (int i = size_of_set-1; i>=0; i--){
+        array[i]=size_of_set - i;
     }
-
-    if (size_of_set >= 0) cout<<"{}"<<endl;
-
     vector<bool> did_i_pick_this(size_of_set);
+
     for(int j = 0; j<size_of_set; j++){
-        did_i_pick_this[j]=true;
         do{
             cout<<"{ ";
-            for(int i = 0;i<size_of_set; i++){
+            for(int i = did_i_pick_this.size()-1;i>=0; i--){
                 if(did_i_pick_this[i]) cout<<array[i]<< " ";
             }
             cout<<"}"<<endl;
-        }while(prev_permutation(did_i_pick_this.begin(),did_i_pick_this.end()));
+        }while(next_permutation(did_i_pick_this.begin(),did_i_pick_this.end()));
+        did_i_pick_this[size_of_set-1-j]=true;
     }
-
+    cout<<"{ ";
+    for(int i = did_i_pick_this.size()-1;i>=0; i--){
+        if(did_i_pick_this[i]) cout<<array[i]<< " ";
+    }
+    cout<<"}"<<endl;
     return 0;
 }
