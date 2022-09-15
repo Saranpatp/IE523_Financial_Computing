189cbc011e4560a9ff2075a313ec699914752d63
Saranpat Prasertthum <saranpatp@gmail.com>
2022-09-14 19:43:07 -0500

    refactor solutions

--- Combinations/main.cpp
+++ Combinations/main.cpp
@@ -11,17 +11,22 @@ int main(int argc,char* argv[]) {
     sscanf(argv[1],"%d",&size_of_array);
     sscanf(argv[2],"%d",&n);
 
+//    size_of_array = 5;
+//    n = 3;
+
     array = new int[size_of_array];
     for (int i = 0; i<size_of_array; i++){
         array[i]=i+1;
     }
-    vector<bool> did_i_pick_this(size_of_array);
-    fill(did_i_pick_this.begin(),did_i_pick_this.begin()+n, true);
-    sort(did_i_pick_this.begin(),did_i_pick_this.end());
+
+    vector<bool> did_i_pick_this(size_of_array,true);
+    fill(did_i_pick_this.begin() ,did_i_pick_this.begin()+n, false);
+
     do{
         cout<<"{ ";
         for(int i = 0;i<size_of_array; i++){
-            if(did_i_pick_this[(size_of_array-1)-i]) cout<<array[i]<< " ";
+            if(!did_i_pick_this[i]) cout<<array[i]<< " ";
+
         }
         cout<<"}"<<endl;
     }while(next_permutation(did_i_pick_this.begin(),did_i_pick_this.end()));
--- PowerSet/main.cpp
+++ PowerSet/main.cpp
@@ -5,30 +5,32 @@
 using namespace std;
 
 int main(int argc,char* argv[]) {
-    int size_of_set;
+    int size_of_array, n;
     int *array;
 
-    sscanf(argv[1],"%d",&size_of_set);
-    array = new int[size_of_set];
-    for (int i = size_of_set-1; i>=0; i--){
-        array[i]=size_of_set - i;
-    }
-    vector<bool> did_i_pick_this(size_of_set);
+    sscanf(argv[1],"%d",&size_of_array);
 
-    for(int j = 0; j<size_of_set; j++){
+//    size_of_array = 5;
+
+    array = new int[size_of_array];
+    for (int i = 0; i<size_of_array; i++){
+        array[i]=i+1;
+    }
+
+    vector<bool> did_i_pick_this(size_of_array,true);
+//    fill(did_i_pick_this.begin() ,did_i_pick_this.begin()+n, false);
+
+    for(int j = 0; j<size_of_array + 1; j++){
+        fill(did_i_pick_this.begin() ,did_i_pick_this.begin()+j, false);
         do{
             cout<<"{ ";
-            for(int i = did_i_pick_this.size()-1;i>=0; i--){
-                if(did_i_pick_this[i]) cout<<array[i]<< " ";
+            for(int i = 0;i<size_of_array; i++){
+                if(!did_i_pick_this[i]) cout<<array[i]<< " ";
+
             }
             cout<<"}"<<endl;
         }while(next_permutation(did_i_pick_this.begin(),did_i_pick_this.end()));
-        did_i_pick_this[size_of_set-1-j]=true;
     }
-    cout<<"{ ";
-    for(int i = did_i_pick_this.size()-1;i>=0; i--){
-        if(did_i_pick_this[i]) cout<<array[i]<< " ";
-    }
-    cout<<"}"<<endl;
+
     return 0;
 }
