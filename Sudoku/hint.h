1c1190fb39776dafb93ee86706c15423aaa307bf
Saranpat Prasertthum <saranpatp@gmail.com>
2022-09-22 18:06:51 -0500

    add sudoku solution

--- Sudoku/hint.h
+++ Sudoku/hint.h
@@ -19,30 +19,38 @@ class Sudoku
 {
     // Private
     int puzzle[9][9];
-    map<int,set<int>> rowsMap;
-    map<int,set<int>> colsMap;
-    map<pair<int,int>,set<int>> boardMap;
 
-    bool row_valid(int row, int value)
-    {
-        return !(rowsMap[row].find(value)!=rowsMap[row].end());
-        // write code that checks if "row" is valid
-    }
+    bool isBoard_valid(int row, int col){
+        set<int> rows;
+        set<int> cols;
+        set<int> board;
+        //check row
+        for (int j = 0; j < 9; j++){
+            int value = puzzle[row][j];
+            if (value==0) continue;
+            if (rows.find(value) != rows.end()) return false;
+            rows.insert(value);
+        }
+        //check col
+        for (int i = 0; i < 9 ; i++){
+            int value = puzzle[i][col];
+            if (value==0) continue;
+            if (cols.find(value) != cols.end()) return false;
+            cols.insert(value);
+        }
 
-    // Private member function that checks if the named column is valid
-    bool col_valid(int col, int value)
-    {
-        return !(colsMap[col].find(value)!=colsMap[col].end());
-        // check validity of "col"
-    }
-
-    // Private member function that checks if the named 3x3 block is valid
-    bool block_valid(int row, int col,int value)
-    {
         int brow = row/3;
         int bcol = col/3;
         // check 3 x 3 block validity
-        return !(boardMap[{brow,bcol}].find(value) != boardMap[{brow,bcol}].end());
+        for (int i = 0 ; i < 3; i++){
+            for (int j = 0 ; j < 3; j++){
+                int value = puzzle[brow*3 + i][bcol*3 + j];
+                if (value == 0) continue;
+                if ( board.find(value) != board.end()) return false;
+                board.insert(value);
+            }
+        }
+        return true;
     }
 
 
@@ -52,23 +60,8 @@ class Sudoku
     // to flag this (see write-up). I am using tuples for this. This
     // way I can show which value is repeated along the diagonal multiple times
     // I took this from https://stackoverflow.com/questions/321068/returning-multiple-values-from-a-c-function
-
     std::tuple<bool, int> positive_diagonals_valid()
     {
-        // add set or map here to check that it is valid
-        set<int> posDiag;
-        //check first half
-        for (int i = 0; i < 9; i++){
-            for (int j = 0; j <= i; j++){
-                if (posDiag.find(puzzle[i][j]) != posDiag.end()) return {false,puzzle[i][j]};
-            }
-        }
-        for (int j = 1; j < 9; j++){
-            for (int i = 9-j; i >= j; i--){
-                if (posDiag.find(puzzle[i][j]) != posDiag.end()) return {false,puzzle[i][j]};
-            }
-        }
-        return {true,0};
         // write code that checks if there are any repeated
         // digits along the 15-many positive diagonals
         // If the intial puzzle does not meet this requirement,
@@ -82,6 +75,19 @@ class Sudoku
         // then the second-tuple is the X that was repeated along
         // some positive diagonal (see description of assignment
         // for a sample output).
+        // add set or map here to check that it is valid
+        //check first half
+        map<int,set<int>> posDiagMap; // nrow+ncol = const
+        for(int i = 0; i < 9; i++){
+            for(int j = 0; j < 9;j++){
+                int value = puzzle[i][j];
+                if (value == 0) continue;
+                if (posDiagMap[i+j].find(value) != posDiagMap[i+j].end()) return {false,value};
+                posDiagMap[i+j].insert(value);
+            }
+        }
+        return {true,0};
+
     }
 
     // Private member funtion that checks if adjacent cells
@@ -97,17 +103,45 @@ class Sudoku
         // then the two ints (i and j, say) identify puzzle[i][j] and
         // puzzle[i-1][j+1] that do not meet the "minimum-difference
         // of 4" (see program assignment description for a sample output)
+        //check first half
+        for(int i = 0; i < 9; i++){
+            for(int j = 0; j < 9;j++){
+                int value = puzzle[i][j];
+                if (value == 0) continue;
+                if (i-1>=0 && j+1<9){
+                    if (puzzle[i-1][j+1] == 0) continue;
+                    if (abs(value - puzzle[i-1][j+1]) <4) return {false,i,j};
+                }
+            }
+        }
+        return {true,0,0};
     }
 
 
+
 public:
+
+
     // Public member function that reads the incomplete puzzle
     // we are not doing any checks on the input puzzle -- that is,
     // we are assuming they are indeed valid
     void read_puzzle(int argc, char * const argv[])
     {
+
         // write code that reads the input puzzle using the
         // guidelines of figure 23 of the bootcamp material
+        int value;
+        ifstream input_file(argv[1]);
+//        ifstream input_file("input5.txt");
+        if (input_file.is_open()){
+            for (int i = 0; i < 9; i++){
+                for (int j = 0; j < 9; j++){
+                    input_file >> value;
+                    puzzle[i][j] = value;
+                }
+            }
+        }
+
     }
 
     // Public member function that prints the puzzle when called
@@ -137,7 +171,7 @@ public:
     bool isPuzzleSloved(int row, int col){
         for(int i = row; i < 9;i++)
             for (int j = col; j < 9; j++)
-                if (puzzle[i][j] != 0) return false;
+                if (puzzle[i][j] == 0) return false;
         return true;
     }
 
@@ -146,9 +180,27 @@ public:
     bool Solve(int row, int col)
     {
         if(isPuzzleSloved(row,col)) return true;
-        for (int k = 1; k <= 9 ; k++){
-            if (row_valid(row,k)&& col_valid(col,k)&&)
+        for (int i = 0 ; i < 9; i++){
+            for (int j = 0; j < 9; j++){
+                if (puzzle[i][j]==0){
+                    for (int k = 1; k <= 9 ; k++){
+                        puzzle[i][j] = k;
+                        if (isBoard_valid(i,j) &&
+                            get<0>(positive_diagonals_valid()) &&
+                            get<0>(adjacent_cells_along_positive_diagonals_have_at_least_a_difference_of_4()) &&
+                            Solve(i,j)
+                            ) {
+                            return true;
+                        }
+                    }
+                    //cannot slove
+                    puzzle[i][j] = 0;
+                    return false;
+                }
+            }
         }
+        return false;
+
         // this part of the code identifies the row and col number of the
         // first incomplete (i.e. 0) entry in the puzzle.  If the puzzle has
         // no zeros, the variable row will be 9 => the puzzle is done, as
